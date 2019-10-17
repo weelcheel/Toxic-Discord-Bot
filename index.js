78f37030dbfb4680d3cd4843e1e368c21b1b3c7d
Weel <arid.ways@gmail.com>
2019-10-16 19:30:10 -0500

    youtube player

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
+const Youtube = require('youtube-audio-stream');
+const Decoder = require('lame').Decoder
+//const Speaker = require('speaker')
+
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
@@ -7,16 +11,20 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
     console.log(message.content);
-    if (message.content.toLowerCase() === 'seth is') {
-        message.reply('malding');
-        
-        var voiceChannel = message.member.voiceChannel;
+
+    let index = message.content.toLowerCase().indexOf('!play');
+    if (index >= 0) {
+        let url = message.content.substring(index + '!play '.length);
+        console.log(url);
+
+        let voiceChannel = message.member.voiceChannel;
         voiceChannel.join().then(connection => {
-            setTimeout(() => {
-                voiceChannel.leave();
-            }, 5000);
+            let decoder = Decoder();
+            Youtube(url).pipe(decoder);
+
+            connection.playConvertedStream(decoder);
         });
     }
 });
 
-bot.login('NjM0MTU5ODAzOTcwNzQ4NDM3.XaemhA.OOYzeYW2O55urAU1UatmT_URsO0');
+bot.login('NjM0MTU5ODAzOTcwNzQ4NDM3.XaewWQ.VNncZKV7de8W4T-xvaiZ3o8cuIE');
